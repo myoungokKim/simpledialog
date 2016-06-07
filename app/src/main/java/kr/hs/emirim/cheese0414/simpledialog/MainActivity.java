d1dff0c77775b425d4ced76559d3be20cd9addd8
Myoungok Kim <cheese0414@naver.com>
2016-06-07 20:08:14 +0900

    #2 Create AlertDialog.Builder

--- app/src/main/java/kr/hs/emirim/cheese0414/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/cheese0414/simpledialog/MainActivity.java
@@ -1,13 +1,33 @@
 package kr.hs.emirim.cheese0414.simpledialog;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but=(Button)findViewById(R.id.but_dialog);
+        but.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("First Dialog");
+        dialog.setMessage("This is message.");
+        dialog.setIcon(R.drawable.first_icon);
+        dialog.setPositiveButton("확인", null);
+        dialog.show();
     }
 }
